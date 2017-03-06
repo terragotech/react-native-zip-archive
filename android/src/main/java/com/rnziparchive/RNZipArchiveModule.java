9027f40f403c372041c9f129eea99997ed754147
praba-kris <praba@knetworks.co.in>
2017-03-06 16:53:29 +0530

    Update zip & Unzip with Promise.

--- android/src/main/java/com/rnziparchive/RNZipArchiveModule.java
+++ android/src/main/java/com/rnziparchive/RNZipArchiveModule.java
@@ -1,5 +1,7 @@
 package com.rnziparchive;
 
+import com.facebook.react.bridge.Promise;
+
 import android.content.res.AssetFileDescriptor;
 import android.util.Log;
 
@@ -10,79 +12,96 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+
 import org.json.JSONObject;
+
 import java.io.File;
 
 
 public class RNZipArchiveModule extends ReactContextBaseJavaModule {
 
-  public RNZipArchiveModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-  }
-
-  @Override
-  public String getName() {
-    return "RNZipArchive";
-  }
-
-  @ReactMethod
-  public void unzip(String zipFilePath, String destDirectory, String password, Callback callback) {
-    JSONObject response = Zip4jArchive.unzip(new File(zipFilePath), destDirectory, password);
-    try{
-      boolean isSuccess = response.getBoolean("isSuccess");
-
-      if(isSuccess) {
-        callback.invoke(null, null);
-      }else {
-        String message = response.getString("response");
-        callback.invoke(makeErrorPayloadFromMessage("Couldn't open file - ", message));
-      }
-    }catch (Exception e){
-      e.printStackTrace();
+    public RNZipArchiveModule(ReactApplicationContext reactContext) {
+        super(reactContext);
     }
-  }
 
-  @ReactMethod
-  public void unzipAssets(String assetsPath, String destDirectory, Callback completionCallback) {
-  }
-
-  @ReactMethod
-  public void zip(String fileOrDirectory, String destDirectory,  String password,Callback callback) {
-    JSONObject response = Zip4jArchive.zip(fileOrDirectory, destDirectory, password);
-    try{
-      boolean isSuccess = response.getBoolean("isSuccess");
-      String message = response.getString("response");
-      callback.invoke(makePayloadResponse(isSuccess, message));
-    }catch (Exception e){
-      e.printStackTrace();
+    @Override
+    public String getName() {
+        return "RNZipArchive";
     }
-  }
 
-  private void zipStream(String[] files, String destFile, long totalSize, Callback completionCallback) {
-    try {
+    @ReactMethod
+    public void unzip(String zipFilePath, String destDirectory, String password, Callback callback) {
+        JSONObject response = Zip4jArchive.unzip(new File(zipFilePath), destDirectory, password);
+        try {
+            boolean isSuccess = response.getBoolean("isSuccess");
 
-    } catch (Exception ex) {
-      ex.printStackTrace();
+            if (isSuccess) {
+                callback.invoke(null, null);
+            } else {
+                String message = response.getString("response");
+                callback.invoke(makeErrorPayloadFromMessage("Couldn't open file - ", message));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-  }
+
+    @ReactMethod
+    public void unzipAssets(String assetsPath, String destDirectory, Promise promise) {
+
+    }
+
+    //zip,unip,zipwith password,unzip with password.
+    @ReactMethod
+    public void zip(String fileOrDirectory, String destDirectory, Promise promise) {
+        JSONObject response = Zip4jArchive.zip(fileOrDirectory, destDirectory, null);
+        try {
+            boolean isSuccess = response.getBoolean("isSuccess");
+            String message = response.getString("response");
+            promise.resolve(makePayloadResponse(isSuccess, message));
+        } catch (Exception e) {
+            promise.reject(null, "Couldn't open file " + fileOrDirectory + ". ");
+            e.printStackTrace();
+        }
+    }
+    @ReactMethod
+    public void zipWithPassword(String fileOrDirectory, String destDirectory, String password, Promise promise) {
+        JSONObject response = Zip4jArchive.zip(fileOrDirectory, destDirectory, password);
+        try {
+            boolean isSuccess = response.getBoolean("isSuccess");
+            String message = response.getString("response");
+            promise.resolve(makePayloadResponse(isSuccess, message));
+        } catch (Exception e) {
+            promise.reject(null, "Couldn't open file " + fileOrDirectory + ". ");
+            e.printStackTrace();
+        }
+    }
+
+    private void zipStream(String[] files, String destFile, long totalSize, Callback completionCallback) {
+        try {
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
 
-  private WritableMap makeErrorPayload(String message, Exception ex) {
-    WritableMap error = Arguments.createMap();
-    error.putString("message", String.format("%s (%s)", message, ex.getMessage()));
-    return error;
-  }
+    private WritableMap makeErrorPayload(String message, Exception ex) {
+        WritableMap error = Arguments.createMap();
+        error.putString("message", String.format("%s (%s)", message, ex.getMessage()));
+        return error;
+    }
 
-  private WritableMap makePayloadResponse(boolean success,String message){
-    WritableMap error = Arguments.createMap();
-    error.putString("message", message);
-    error.putBoolean("success",success);
-    return error;
-  }
+    private WritableMap makePayloadResponse(boolean success, String message) {
+        WritableMap error = Arguments.createMap();
+        error.putString("message", message);
+        error.putBoolean("success", success);
+        return error;
+    }
 
-  private WritableMap makeErrorPayloadFromMessage(String message, String ex) {
-    WritableMap error = Arguments.createMap();
-    error.putString("message", String.format("%s (%s)", message, ex));
-    return error;
-  }
+    private WritableMap makeErrorPayloadFromMessage(String message, String ex) {
+        WritableMap error = Arguments.createMap();
+        error.putString("message", String.format("%s (%s)", message, ex));
+        return error;
+    }
 }
--- android/src/main/java/com/rnziparchive/Zip4jArchive.java
+++ android/src/main/java/com/rnziparchive/Zip4jArchive.java
@@ -1,11 +1,14 @@
 package com.rnziparchive;
 
 import android.os.Environment;
+
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.model.UnzipParameters;
 import net.lingala.zip4j.model.ZipParameters;
 import net.lingala.zip4j.util.Zip4jConstants;
+
 import org.json.JSONObject;
+
 import java.io.File;
 
 /**
@@ -15,42 +18,42 @@ import java.io.File;
 public class Zip4jArchive {
     /**
      * unzip password protected zip files
+     *
      * @param compressedFile
      * @param destinationPath
      */
-    public static JSONObject unzip(File compressedFile,String destinationPath,String password){
+    public static JSONObject unzip(File compressedFile, String destinationPath, String password) {
         JSONObject jsonObject = new JSONObject();
         boolean isSuccess = true;
         String response = "";
         try {
             ZipFile zipFile = new ZipFile(compressedFile);
-            if(zipFile.isEncrypted()){
+            if (zipFile.isEncrypted()) {
                 zipFile.setPassword(password);
             }
             zipFile.extractAll(destinationPath);
             compressedFile.delete();
             isSuccess = true;
             response = destinationPath;
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             isSuccess = false;
             response = e.getMessage();
         }
-        try{
+        try {
             jsonObject.put("isSuccess", isSuccess);
             jsonObject.put("response", response);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return jsonObject;
     }
 
     /**
-     *
      * @param sourcePath
      * @param destPathWithName
      */
-    public static JSONObject zip(String sourcePath,String destPathWithName,String password){
+    public static JSONObject zip(String sourcePath, String destPathWithName, String password) {
         JSONObject jsonObject = new JSONObject();
         try {
             File sourceFolder = new File(sourcePath);
@@ -58,32 +61,34 @@ public class Zip4jArchive {
             ZipParameters parameters = new ZipParameters();
             parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
             parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
-            parameters.setEncryptFiles(true);
-            parameters.setEncryptionMethod(Zip4jConstants.ENC_METHOD_STANDARD);
-            parameters.setPassword(password);
-            if(sourceFolder != null){
+            if (password != null && !password.trim().isEmpty()) {
+                parameters.setEncryptFiles(true);
+                parameters.setEncryptionMethod(Zip4jConstants.ENC_METHOD_STANDARD);
+                parameters.setPassword(password);
+            }
+            if (sourceFolder != null) {
                 File[] sourceFiles = sourceFolder.listFiles();
                 for (File sourceFile : sourceFiles) {
                     if (sourceFile.isFile()) {
                         zipFile.addFile(sourceFile, parameters);
                     } else {
-                        zipFile.addFolder(sourceFile,parameters);
+                        zipFile.addFolder(sourceFile, parameters);
                     }
                 }
                 jsonObject.put("isSuccess", true);
                 jsonObject.put("response", "success");
-            }else{
+            } else {
                 jsonObject.put("isSuccess", false);
                 jsonObject.put("response", "Source folder not found.");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-               try{
-                   jsonObject.put("isSuccess", false);
-                   jsonObject.put("response", "Error.");
-               }catch (Exception e1){
+            try {
+                jsonObject.put("isSuccess", false);
+                jsonObject.put("response", "Error.");
+            } catch (Exception e1) {
 
-               }
+            }
 
         }
         return jsonObject;
